d5141d2edbae9f1467489ee7c5314302f2eadbbb
Ahmed Mesbah <ahmed.320230033@ejust.edu.eg>
2026-08-09 02:42:16 +0300

    fix(crash): make LoggerApp extend NavigatorAppBase to resolve ClassCastException when selecting space

--- clients/android-new/logger/src/main/java/cy/ac/ucy/cs/anyplace/logger/LoggerApp.kt
+++ clients/android-new/logger/src/main/java/cy/ac/ucy/cs/anyplace/logger/LoggerApp.kt
@@ -1,11 +1,11 @@
 package cy.ac.ucy.cs.anyplace.logger
 
-import cy.ac.ucy.cs.anyplace.lib.android.AnyplaceApp
+import cy.ac.ucy.cs.anyplace.lib.android.NavigatorAppBase
 import cy.ac.ucy.cs.anyplace.lib.android.utils.LOG
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class LoggerApp : AnyplaceApp() {
+class LoggerApp : NavigatorAppBase() {
 
   override fun onCreate() {
     super.onCreate()
